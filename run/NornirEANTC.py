eac421b84a1a29e40da49500460a47db562e4e37
ankudinov <petr.ankudinov@gmail.com>
2024-01-31 17:24:28 +0000

    test nornir

--- run/NornirEANTC.py
+++ run/NornirEANTC.py
@@ -11,5 +11,13 @@ class Otnorok:
 
     def __init__(self):
         self.nr = InitNornir(
-            runner={'plugin': 'threaded', 'options': {'num_workers': 20}}
+            runner={'plugin': 'threaded', 'options': {'num_workers': 20}},
+            inventory={
+                'plugin': 'SimpleInventory',
+                'options': {
+                    'host_file': f'{os.getcwd()}/eantc2024/testbeds/evpn-vxlan/nornir-hosts.yml',
+                    'group_file': f'{os.getcwd()}/nornir-groups.yml',
+                    'defaults_file': f'{os.getcwd()}/nornir-defaults.yml'
+                }
+            }
         )
