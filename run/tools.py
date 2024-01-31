eca40ecc56c269076ed4b1ff97834539d9a2a957
ankudinov <petr.ankudinov@gmail.com>
2024-01-31 12:46:07 +0000

    add timestamp

--- run/tools.py
+++ run/tools.py
@@ -3,6 +3,22 @@ import yaml
 import csv
 
 
+def time_stamp():
+    """
+    time_stamp function returns current system time as Y-M-D H-M-S string
+
+    Args:
+        no arguments required
+
+    Returns:
+        str: current system time as Y-M-D H-M-S string
+    """
+    time_not_formatted = time()
+    time_formatted = datetime.fromtimestamp(
+        time_not_formatted).strftime('%Y-%m-%d:%H:%M:%S.%f')
+    return time_formatted
+
+
 def read_yaml_file(filename, load_all=False):
     with open(filename, mode='r') as f:
         if not load_all:
