24aba910c585d27b2466277a0f1b8f77acd613b6
ankudinov <petr.ankudinov@gmail.com>
2024-01-31 13:44:07 +0000

    modify cli

--- run/cli.py
+++ run/cli.py
@@ -12,7 +12,7 @@ def interpreter():
         help='The EANTC year. Optional, default is 2024. This will be used to find tests parent directory.'
     )
     parser.add_argument(
-        '-c', '--category', default='',
+        '-c', '--category', choices=['evpn-vxlan', 'evpn-mpls', 'sr-mpls'],
         help='EANTC test category: evpn-vxlan, evpn-mpls, sr-mpls'
     )
     parser.add_argument(
