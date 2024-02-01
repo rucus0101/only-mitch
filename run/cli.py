3a6b4b9e5133752ee2a253cd6fc6d5b8a367c5e2
ankudinov <petr.ankudinov@gmail.com>
2024-02-01 17:35:34 +0000

    fix issue with dir create

--- run/cli.py
+++ run/cli.py
@@ -12,13 +12,7 @@ import re
 from run.tools import time_stamp
 
 
-def get_config(task: Task, test_dir_full_path: str, current_time='') -> Result:
-    if current_time:
-        out_dir = os.path.join(test_dir_full_path, f'configs_{current_time}')
-    else:
-        out_dir = os.path.join(test_dir_full_path, f'configs')
-    if not os.path.isdir(out_dir):
-        os.makedirs(out_dir)
+def get_config(task: Task, cfg_dir: str) -> Result:
     result = task.run(
         task=napalm_get,
         getters=['config']
@@ -26,42 +20,24 @@ def get_config(task: Task, test_dir_full_path: str, current_time='') -> Result:
     task.run(
         task=write_file,
         content=result.result['config']['running'],
-        filename=f'{out_dir}/{task.host}.cfg'
+        filename=f'{cfg_dir}/{task.host}.cfg'
     )
     print('Saved ' + f'configs/{task.host}.cfg')
 
 
-def run_a_command_list(task: Task, a_command_list: list, test_dir_full_path: str, current_time=''):
-    if current_time:
-        out_dir = os.path.join(test_dir_full_path, f'test_results_{current_time}')
-    else:
-        out_dir = os.path.join(test_dir_full_path, f'test_results')
-    if not os.path.isdir(out_dir):
-        os.makedirs(out_dir)
+def run_a_command_list(task: Task, cmds_and_dirnames: list):
     result = task.run(
         task=napalm_cli,
-        commands=a_command_list
+        commands=[cmd[0] for cmd in cmds_and_dirnames]
     )
-    for a_command in a_command_list:
-        # find all words in a command to remove all non-printable characters
-        word_list = re.findall(r"[\w]+", a_command)
-        # build a file name for show command output
-        cmd_line = ''
-        while word_list:
-            cmd_line += word_list.pop(0)
-            if word_list:  # if not the last word, add separator
-                cmd_line += '-'
-        # create subdirectory for cli command to be collected
-        cmd_out_dir = os.path.join(out_dir, cmd_line)
-        if not os.path.isdir(cmd_out_dir):
-            os.makedirs(cmd_out_dir)
-        # write a command output to a file
+    # write a command output to a file
+    for cmd, cmd_dirname in cmds_and_dirnames:
         task.run(
             task=write_file,
-            content=result.result[a_command],
-            filename=f'{cmd_out_dir}/{task.host}.txt'
+            content=result.result[cmd],
+            filename=f'{cmd_dirname}/{task.host}.txt'
         )
-        print('Saved ' + f'{cmd_out_dir}/{task.host}.txt')
+        print('Saved ' + f'{cmd_dirname}/{task.host}.txt')
 
 
 def interpreter():
@@ -88,12 +64,54 @@ def interpreter():
     )
     argcomplete.autocomplete(parser)
     args = parser.parse_args()
-    
+
+    # create the required directories outside Nornir to avoid issues with threading
     # check if test directory exists
     test_dir_name = f'eantc{args.year}/testbeds/{args.inventory}/tests/{args.task}'
     test_dir_full_path = os.path.join(os.getcwd(), test_dir_name)
     if not os.path.isdir(test_dir_full_path):
         sys.exit(f'ERROR: {test_dir_full_path} does not exist!')
+    # build time stamp if required
+    if args.no_timestamp:
+        current_time = ''
+    else:
+        current_time = time_stamp()
+    # create config dir
+    if current_time:
+        config_dir = os.path.join(test_dir_full_path, f'configs_{current_time}')
+    else:
+        config_dir = os.path.join(test_dir_full_path, f'configs')
+    if not os.path.isdir(config_dir):
+        os.makedirs(config_dir)
+    # create outpit dir for test commands
+    if current_time:
+        test_out_dir = os.path.join(test_dir_full_path, f'test_results_{current_time}')
+    else:
+        test_out_dir = os.path.join(test_dir_full_path, f'test_results')
+    if not os.path.isdir(test_out_dir):
+        os.makedirs(test_out_dir)
+
+    # build a list of show commands
+    snapshot_command_list = list()
+    with open(f'{test_dir_full_path}/test-commands.txt', 'r') as snapshot_commands_file:
+        snapshot_command_list = [a_line.strip() for a_line in snapshot_commands_file]
+
+    cmd_list_with_dirnames = list()
+    for a_command in snapshot_command_list:
+        # find all words in a command to remove all non-printable characters
+        word_list = re.findall(r"[\w]+", a_command)
+        # build prefix for a show command to be added to the filename or directory name
+        cmd_line = ''
+        while word_list:
+            cmd_line += word_list.pop(0)
+            if word_list:  # if not the last word, add separator
+                cmd_line += '-'
+        cmd_list_with_dirnames.append((a_command, os.path.join(test_out_dir, cmd_line)))
+        # create subdirectory for cli command to be collected
+        cmd_out_dir = os.path.join(test_out_dir, cmd_line)
+        if not os.path.isdir(cmd_out_dir):
+            os.makedirs(cmd_out_dir)
+
     # init Nornir
     nr = InitNornir(
         runner={'plugin': 'threaded', 'options': {'num_workers': 20}},
@@ -106,19 +124,12 @@ def interpreter():
             }
         }
     )
-    # current time must be set outside of Nornir task to have single time stamp
-    if args.no_timestamp:
-        current_time = ''
-    else:
-        current_time = time_stamp()
+    
     # collect configs
-    result = nr.run(task=get_config, test_dir_full_path=test_dir_full_path, current_time=current_time)
+    result = nr.run(task=get_config, cfg_dir=config_dir)
     if result.failed:
         print(f'ERROR: Failed to collect configs from the following hosts: {[k for k in result.failed_hosts.keys()]}')
     # collect show comands
-    snapshot_command_list = list()
-    with open(f'{test_dir_full_path}/test-commands.txt', 'r') as snapshot_commands_file:
-        snapshot_command_list = [a_line.strip() for a_line in snapshot_commands_file]
-    result = nr.run(task=run_a_command_list, a_command_list=snapshot_command_list, test_dir_full_path=test_dir_full_path, current_time=current_time)
+    result = nr.run(task=run_a_command_list, cmds_and_dirnames=cmd_list_with_dirnames)
     if result.failed:
         print(f'ERROR: Failed to collect show commands from the following hosts: {[k for k in result.failed_hosts.keys()]}')
