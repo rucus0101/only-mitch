dff1a549b33cdb124ddeee639a244eb407ce0cb5
ankudinov <petr.ankudinov@gmail.com>
2024-02-01 16:52:53 +0000

    init collecting show commands

--- run/cli.py
+++ run/cli.py
@@ -8,6 +8,7 @@ from nornir_napalm.plugins.tasks import napalm_cli
 from nornir.core.task import Task, Result
 import os
 import sys
+import re
 from run.tools import time_stamp
 
 
@@ -28,6 +29,39 @@ def get_config(task: Task, test_dir_full_path: str, current_time='') -> Result:
         filename=f'{out_dir}/{task.host}.cfg'
     )
     print('Saved ' + f'configs/{task.host}.cfg')
+
+
+def run_a_command_list(task: Task, a_command_list: list, test_dir_full_path: str, current_time=''):
+    if current_time:
+        out_dir = os.path.join(test_dir_full_path, f'test_results_{current_time}')
+    else:
+        out_dir = os.path.join(test_dir_full_path, f'test_results')
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
+    result = task.run(
+        task=napalm_cli,
+        commands=a_command_list
+    )
+    for a_command in a_command_list:
+        # find all words in a command to remove all non-printable characters
+        word_list = re.findall(r"[\w]+", a_command)
+        # build a file name for show command output
+        cmd_line = ''
+        while word_list:
+            cmd_line += word_list.pop(0)
+            if word_list:  # if not the last word, add separator
+                cmd_line += '-'
+        # create subdirectory for cli command to be collected
+        cmd_out_dir = os.path.join(out_dir, cmd_line)
+        if not os.path.isdir(cmd_out_dir):
+            os.makedirs(cmd_out_dir)
+        # write a command output to a file
+        task.run(
+            task=write_file,
+            content=result.result[a_command],
+            filename=f'{cmd_out_dir}/{task.host}.txt'
+        )
+        print('Saved ' + f'{cmd_out_dir}/{task.host}.txt')
 
 
 def interpreter():
@@ -81,3 +115,10 @@ def interpreter():
     result = nr.run(task=get_config, test_dir_full_path=test_dir_full_path, current_time=current_time)
     if result.failed:
         print(f'ERROR: Failed to collect configs from the following hosts: {[k for k in result.failed_hosts.keys()]}')
+    # collect show comands
+    snapshot_command_list = list()
+    with open(f'{test_dir_full_path}/test-commands.txt', 'r') as snapshot_commands_file:
+        snapshot_command_list = [a_line.strip() for a_line in snapshot_commands_file]
+    result = nr.run(task=run_a_command_list, a_command_list=snapshot_command_list, test_dir_full_path=test_dir_full_path, current_time=current_time)
+    if result.failed:
+        print(f'ERROR: Failed to collect show commands from the following hosts: {[k for k in result.failed_hosts.keys()]}')
